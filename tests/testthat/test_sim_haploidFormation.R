3ad78e083277a8ddfca703a78153f62a88d79ed7
cnieuwoudt <cnieuwou@sfu.ca>
2017-05-15 10:49:20 -0700

    add test function for sim_gameteFormation

--- tests/testthat/test_sim_haploidFormation.R
+++ tests/testthat/test_sim_haploidFormation.R
@@ -2,7 +2,9 @@ library(testthat)
 context("sim_hapliodFormation")
 test_that("sim_hapliodFormation has ncols = length(chiasmata positions) + 2", {
 
-  my_chrom_map <- data.frame(start = 0, stop = 250, center = 50)
+  my_chrom_map <- data.frame(start = round(runif(1, 0, 500)))
+  my_chrom_map$stop <- my_chrom_map$start + round(runif(1, 50, 300))
+  my_chrom_map$center <- round(runif(1, my_chrom_map$start + 1, my_chrom_map$stop -1))
   sim_chias_pos <- sim_chiasmataPositions(my_chrom_map)
   chias_count_BC(sim_chias_pos, my_chrom_map[1,3])
   my_haps <- sim_haploidFormation(num_chiasmata = length(sim_chias_pos),
@@ -14,7 +16,9 @@ test_that("sim_hapliodFormation has ncols = length(chiasmata positions) + 2", {
 
 test_that("sim_hapliodFormation always returns a unique gamete group for each recombined haploid", {
 
-  my_chrom_map <- data.frame(start = 0, stop = 250, center = 50)
+  my_chrom_map <- data.frame(start = round(runif(1, 0, 500)))
+  my_chrom_map$stop <- my_chrom_map$start + round(runif(1, 50, 300))
+  my_chrom_map$center <- round(runif(1, my_chrom_map$start + 1, my_chrom_map$stop -1))
   sim_chias_pos <- sim_chiasmataPositions(my_chrom_map)
   chias_count_BC(sim_chias_pos, my_chrom_map[1,3])
   my_haps <- sim_haploidFormation(num_chiasmata = length(sim_chias_pos),
