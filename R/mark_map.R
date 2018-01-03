fcb4ab9ddb466841a61e43061ab3ad85c4557692
cnieuwoudt <cnieuwou@sfu.ca>
2018-01-03 13:51:26 -0800

    added mark_map dataset

---
+++ R/mark_map.R
@@ -0,0 +1,16 @@
+#' Marker Map Dataset
+#'
+#' Example marker map data frame, used to illustrate usage of the \code{\link{markerMap}} function.
+#'
+#' @docType data
+#'
+#' @format A data set with 14 rows and 4 variables:
+#' \describe{
+#'   \item{chrom}{Numeric. The chromosome on which the marker resides.}
+#'   \item{position}{The location of the marker, in basepairs.}
+#'   \item{pathwayID}{A pathwayID for the variant.}
+#'  \item{possibleRV}{Logical. Indicates whether the variant may be chosen as a causal familial variant.}
+#' }
+#'
+#'
+"mark_map"
