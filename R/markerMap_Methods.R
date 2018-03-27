2542041a882114b0d402bf552a9f0d202ed04fe0
cnieuwoudt <cnieuwou@sfu.ca>
2018-03-27 10:07:28 -0700

    Edited markermap documentation

--- R/markerMap_Methods.R
+++ R/markerMap_Methods.R
@@ -7,8 +7,7 @@
 #'  \item{marker}{(OPTIONAL) An identifying name/number for the variant. (If missing this field will be automatically generated from the columns named "chrom" and "position" as "chrom_position"). NOTE: These names must match \emph{exactly} the column names (i.e. the marker names) in the haplotype_dist dataframe.}
 #'  \item{chrom}{The number of the chromosome on which the marker resides.}
 #'  \item{position}{The location of the variant, in basepairs.}
-#'  \item{pathwayID}{A pathwayID for the variant. (**Should be optional, I don't think we use this yet**)}
-#'  \item{possibleRV}{(OPTIONAL) Logical variable, indicates whether the variant may be chosen as a causal familial variant.  If this field is missing, it is assumed that all variants listed are candidates for selection.}
+#'  \item{possibleRV}{(OPTIONAL) Logical variable, indicates whether the variant may be chosen as a causal familial variant.  If this field is missing and \code{pathwayDF} is not provided it is assumed that all variants listed are candidates for selection.}
 #'  \item{probCausal}{(OPTIONAL) The probability that the variant is selected as a familial RV.  If this field is missing, it will be assumed that all variants for which possibleRV is TRUE are equally likely.}
 #' }
 #'
@@ -38,11 +37,20 @@ markerMap <- function(markerDF, pathwayDF = NULL) {
     stop('please provide a data.frame with named variables: chrom and position')
   }
 
+  if (any(is.na(markerDF))) {
+    stop('markerDF contains missing values')
+  }
+
+  if (any(!is_int(markerDF$chrom))) {
+    stop('chromosome numbers must be expressed as integers')
+  }
+
   #add marker name, if not provided
   if (is.null(markerDF$marker)) {
     markerDF$marker <- paste0(markerDF$chrom, sep = "_", markerDF$position)
   }
 
+  #create possibleRV
   if (is.null(pathwayDF) & is.null(markerDF$possibleRV)) {
     markerDF$possibleRV <- TRUE
   } else if (!is.null(pathwayDF)) {
@@ -60,15 +68,6 @@ markerMap <- function(markerDF, pathwayDF = NULL) {
   if (is.null(markerDF$probCausal)) {
     markerDF$probCausal <- markerDF$possibleRV/sum(markerDF$possibleRV)
   }
-
-  if (any(is.na(markerDF))) {
-    stop('markerDF contains missing values')
-  }
-
-  if (any(!is_int(markerDF$chrom))) {
-    stop('chromosome numbers must be expressed as whole numbers')
-  }
-
 
   obj <- markerDF
   class(obj) <- c("markerMap", class(markerDF))
