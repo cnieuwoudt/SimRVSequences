73490c91ff7d8b94e827df684f38b07f0e3d647e
cnieuwoudt <cnieuwou@sfu.ca>
2018-04-13 08:48:22 -0700

    added EXgen and EXmut datasets

---
+++ R/EXmut.R
@@ -0,0 +1,17 @@
+#' Example Mutations Dataset
+#'
+#' This data set contains mutations along segments of length 20,000 base pairs from human chromosomes 1-5. This dataset is intended to accompany the EXgen dataset and is used for demonstration purposes.
+#'
+#' @docType data
+#'
+#' @seealso EXgen
+#'
+#' @format A data set with 173 rows and 4 variables:
+#' \describe{
+#'   \item{colID}{Numeric. The corresponding column number of the SVN in the EXgen dataset.}
+#'   \item{chrom}{Numeric. The choromosome number.}
+#'   \item{position}{Numeric. The location of the SNV, in base pairs.}
+#'   \item{afreq}{Numeric. The derived allele frequency of the SNV.}
+#'   \item{marker}{Character. The names of the genes contained in the combined exon.}
+#' }
+"EXmut"
