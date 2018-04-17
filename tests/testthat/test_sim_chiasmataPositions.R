150bc1be3061ec70f4a81a8da364676d50e15a08
cnieuwoudt <cnieuwou@sfu.ca>
2018-04-17 12:29:43 -0700

    added test for sim_chiasmataPos

--- tests/testthat/test_sim_chiasmataPositions.R
+++ tests/testthat/test_sim_chiasmataPositions.R
@@ -18,8 +18,9 @@ test_that("sim_chiasmataPositions should never return a chiasmata position outsi
 
 test_that("successive chiasmata locations returned by sim_chiasmataPositions should always be strictly increasing", {
 
+  #simualte chiasmata until at least 2 are formed
   B <- 0
-  while (B == 0) {
+  while (B < 2) {
     my_endPoints <- data.frame(start = round(runif(1, 0, 500)))
     my_endPoints$stop <- my_endPoints$start + round(runif(1, 50, 300))
 
@@ -28,4 +29,15 @@ test_that("successive chiasmata locations returned by sim_chiasmataPositions sho
   }
 
   expect_true(all(diff(ex_chiasPos) >= 0))
+})
+
+
+test_that("If start_pos = end_pos no chiasmata are formed", {
+
+    my_endPoints <- data.frame(start = 1,
+                               stop = 1)
+
+    ex_chiasPos <- sim_chiasmataPositions(chrom_map = my_endPoints)
+
+    expect_equal(length(ex_chiasPos), 0)
 })
