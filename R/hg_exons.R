fb4f36fff88950738d48c2b53e6a1c69300cb474
cnieuwoudt <cnieuwou@sfu.ca>
2018-04-24 09:11:28 -0700

    fixed 0-based start in hg_exons and hg_apopPath, changed geneName to NCBIref and updated documentation to reflect the correct variable name

--- R/hg_exons.R
+++ R/hg_exons.R
@@ -1,16 +1,19 @@
 #' Human Exon Data
 #'
-#' This data set catalouges the positions, in base pairs, of the exons residing on human chromosomes 1-22.  The positions of the exons were obtained from the UCSC Genome Broswer, from the  using the NCBI RefSeq curated exons.  In this dataset we have combined overlapping exons into a single entry.
+#' This data set catalouges the positions, in base pairs, of the exons residing on human chromosomes 1-22.  The positions of the exons were obtained from the UCSC Genome Broswer, using the hg38 database and ncbiRefSeqCurated table.  In this dataset we have combined overlapping exons into a single entry.
 #'
 #' NOTE: (REMOVE BEFORE RELEASE) CombineExons_NCBI.R contains details
 #'
 #' @docType data
+#'
+#' @references Karolchik, D., Hinrichs, A. S., Furey, T. S., Roskin, K. M., Sugnet, C. W., Haussler, D., and Ken, W. J. (2004). The UCSC Table Browser data retrieval tool. \emph{Nucleic Acids Res}.
+#' @references Kent, W. J., Sugnet, C. W., Furey, T. S., Roskin, K. M., Pringle, T. H., Zahler, A. M., and Haussler, D. (2002). The human genome browser at UCSC. \emph{Genome Res}, 12(6):996-1006.
 #'
 #' @format A data set with 223289 rows and 4 variables:
 #' \describe{
 #'   \item{chrom}{Numeric. The chromosome number.}
 #'   \item{exonStart}{Numeric. The exon's starting position, in base pairs.}
 #'   \item{exonStop}{Numeric. The exon's ending position, in base pairs.}
-#'   \item{NCBIref}{Character. A list of NCBI reference sequence accession numbers.  Overlapping exons that have been combined into a single observation will  for associated with the exons in the observation.}
+#'   \item{NCBIref}{Character. The NCBI reference seqence accession number of the gene(s) in which the exon(s) reside.}
 #' }
 "hg_exons"
