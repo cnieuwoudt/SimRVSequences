5c45b1006874ce65c033a482c40a744ac6aafc34
cnieuwoudt <cnieuwou@sfu.ca>
2018-10-17 13:22:38 -0700

    create sproadic family in study_peds for examples

--- data-raw/study_peds.R
+++ data-raw/study_peds.R
@@ -5,6 +5,7 @@ library(SimRVPedigree)
 data(AgeSpecific_Hazards)
 my_HR = hazard(AgeSpecific_Hazards)
 
+# load libraries needed to simulate pedigrees in parallel.
 library(doParallel)
 library(doRNG)
 
@@ -14,33 +15,38 @@ cl <- makeCluster(5)   # create cluster
 registerDoParallel(cl) # register cluster
 
 
-start_time <- Sys.time()
+#simulate a sample of five pedigrees using foreach
 study_peds = foreach(i = seq(npeds), .combine = rbind,
-                  .packages = c("kinship2", "SimRVPedigree"),
-                  .options.RNG = 844090518
+                     .packages = c("SimRVPedigree"),
+                     .options.RNG = 84405180
 ) %dorng% {
   # Simulate pedigrees ascertained for at least three disese-affected individuals,
   # according to the age-specific hazard rates in the `AgeSpecific_Hazards` data
-  # set, ascertained from 1980 to 2018, with seed-founder birth year spanning
+  # set, ascertained from 1980 to 2010, with seed-founder birth year spanning
   # from 1900 to 1920, stop year set to 2018, and with genetic relative-risk 50.
   sim_RVped(hazard_rates = my_HR,
             GRR = 50, FamID = i,
             RVfounder = TRUE,
             founder_byears = c(1900, 1920),
-            ascertain_span = c(1980, 2018),
+            ascertain_span = c(1980, 2010),
             stop_year = 2018,
             recall_probs = c(1, 0.5, 0),
             num_affected = 3)[[2]]}
-stop_time <- Sys.time()
-stopCluster(cl)
+
+stopCluster(cl) #shut down cluster
 
 difftime(stop_time, start_time, units = "mins")
 
+# win.graph(h = 20, w = 35)
 # par(mfrow = c(2, 3))
-# plot(study_peds[study_peds$FamID == 1,])
-# plot(study_peds[study_peds$FamID == 2,])
-# plot(study_peds[study_peds$FamID == 3,])
-# plot(study_peds[study_peds$FamID == 4,])
-# plot(study_peds[study_peds$FamID == 5,])
+# plot(study_peds[study_peds$FamID == 1,], ref_year = 2018)
+# plot(study_peds[study_peds$FamID == 2,], ref_year = 2018)
+# plot(study_peds[study_peds$FamID == 3,], ref_year = 2018)
+# plot(study_peds[study_peds$FamID == 4,], ref_year = 2018)
+# plot(study_peds[study_peds$FamID == 5,], ref_year = 2018)
+
+#create a sporadic pedigree for example in vignette
+study_peds$DA2[study_peds$FamID == 2] = 0
 
 save(study_peds, file="data/study_peds.rdata", compress='xz')
+getwd()
