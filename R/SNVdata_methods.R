b6e7d07f95a16ca3e7a1b6cb4a1cfb2060c46e80
cnieuwoudt <cnieuwou@sfu.ca>
2019-06-13 16:24:38 -0700

    add import SNV data

--- R/SNVdata_methods.R
+++ R/SNVdata_methods.R
@@ -19,3 +19,49 @@ SNVdata <- function(SNV_data) {
 is.SNVdata <- function(x) {
   return(inherits(x, "SNVdata"))
 }
+
+#' Import 1000 genomes exon data formatted for sim_RVstudy
+#'
+#' @param chrom The chromosome number.  Can be a numeric list of chromosome numbers or a character argument set to \code{"ALL"} if data from all chromosomes should be imported.
+#'
+#' @return An object of class \code{SNVdata} or a list of objects of class \code{SNVdata}, i.e. one for each chomosome that was imported.
+#' @export
+#'
+#' @examples
+#' exdata = import_SNVdata(19)
+#'
+#' head(SNVdata_chrom21$Mutations)
+#' SNVdata_chrom21$Haplotypes[1:20, 1:10]
+import_SNVdata <- function(chrom){
+
+  if (any(chrom == 0)){
+    chrom = seq(1:22)
+  } else if (!(class(chrom) %in% c("numeric", "integer"))){
+    stop("\n chrom must be a numeric list of chromosome numbers.\n
+           Note if chrom = 0, all autosomes are imported (i.e. chroms 1 - 22 )")
+  }
+
+  #store the object names for the imported data by chromosome number
+  object_names <- paste0("SNVdata_chrom", chrom)
+
+
+  #import the formatted date from github
+  for (i in 1:length(chrom)){
+    load(url(paste0("https://github.com/cnieuwoudt/1000-Genomes-Exon-Data/raw/master/Formatted-SNVdata/SNVdata_chrom",
+                    chrom[i], ".rda", sep = "")))
+  }
+
+  #store each to SNVdata object to a list
+  chrom_dat = lapply(object_names, function(x){get(x)})
+
+  if (length(chrom) > 1){
+    #combine the data from the different chromosomes, for ease of use
+    Haplotypes <- do.call(cbind, lapply(chrom_dat, `[[`, 1))
+    Mutations <- do.call(rbind, lapply(chrom_dat, `[[`, 2))
+  } else {
+    Haplotypes <- chrom_dat$Haplotypes
+    Mutations <- chrom_dat$Mutations
+  }
+
+  return(SNVdata(list(Haplotypes = Haplotypes, Mutations = Mutations)))
+}
