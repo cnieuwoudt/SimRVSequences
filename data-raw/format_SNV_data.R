ef45b9963c0cac4478d922305a8467d0b89483c0
cnieuwoudt <cnieuwou@sfu.ca>
2019-07-27 09:49:22 -0700

    fix allele frequency and quality check samples

--- data-raw/format_SNV_data.R
+++ data-raw/format_SNV_data.R
@@ -6,6 +6,10 @@ library(vcfR)
 #These are the individuals we have sampled who are unrelated
 SampleData <- read.csv("~/GitHub/1000-Genomes-Exon-Data/Formatted-SNVdata/SampleData.csv", stringsAsFactors=FALSE)
 
+sampLength <- c()
+SampID_order <- list()
+dimHaplotypes <- list()
+
 #set the chromosome number of the exon data to import
 chrom_num = 22
 
@@ -26,12 +30,12 @@ genotypes <- vcf@gt[, -1]
 #reduce the genotypes to contain only the individuals we are retaining
 #NOTE: people are columns.
 genotypes <- genotypes[, which(colnames(genotypes) %in% SampleData$Sample)]
-dim(genotypes)
+
+sampLength[[chrom_num]] = dim(genotypes)[2]
+SampID_order[[chrom_num]] = unique(colnames(genotypes))
 
 #convert genotypes to sparseMatrix format
 Haplotypes <- genos2sparseMatrix(genotypes)
-dim(Haplotypes)
-Haplotypes[1:10, 1:10]
 
 #------------------------------#
 # extract and format Mutations #
@@ -44,15 +48,18 @@ Haplotypes[1:10, 1:10]
 muts <- vcfR2tidy(vcf, info_only = TRUE,
                   info_fields = c("AF", "AC", "NS", "AN",
                                   "EAS_AF", "EUR_AF", "AFR_AF", "AMR_AF", "SAS_AF",
-                                  "VT", "EX_TARGET", "DP"),
+                                  "DP"),
                   info_types = c(AF = "n", AC = "i", NS = "i", AN = "i",
                                  EAS_AF = "n", EUR_AF = "n", AFR_AF = "n", AMR_AF = "n", SAS_AF = "n",
                                  DP = "i"))
 
 
 #muts$meta contains variable descriptions for the INFO variables
-muts$meta
+#muts$meta
 
+
+muts$fix$AF <- muts$fix$AC/muts$fix$AN
+#head(muts$fix)
 
 #store as dataframe, remove columns that contain only NA values,
 #and create colID variable to identify the column positon of the mutation
@@ -60,41 +67,38 @@ muts$meta
 Mutations <- as.data.frame(muts$fix)
 Mutations <- cbind(seq(1:dim(Haplotypes)[2]),
                    Mutations[, sapply(1:ncol(Mutations), function(x){any(!is.na(Mutations[, x]))})])
-head(Mutations)
-
-#remove unnecessary columns ???, i.e. FILTER, EX_TARGET, and VT
-#From metadata:
-#EX_TARGET indicates whether a variant is within the exon pull down target boundaries
-#VT indicates what type of variant the line represents
-unique(Mutations$VT)
-unique(Mutations$FILTER)
-
+#head(Mutations)
 
 #rename columns for consistency with read_slim output
 #According to muts$meta the variable "AF" is the estimated allele freq,
 #i.e. the variable we previously named "afreq" in the Mutations output
 #returned by read_slim.
 colnames(Mutations)[c(1:3, 7)] = c("colID", "chrom", "position", "afreq")
-head(Mutations)
+#head(Mutations)
 
-#Chrom must be a numeric variable.
-#Convert chrom to numeric
-Mutations$chrom <- as.numeric(Mutations$chrom)
 
-#How rare are the SNVs in the exported exon data?
-nrow(Mutations)
-length(which(Mutations$afreq <= 0.01))
+# Note that some of the SNVs have an allele count of zero, i.e. AC = 0.
+# Meaning, no individual in the sample carries the SNV.  Also, note that
+# since we removed 22 third order relatives, any SNVs that were carried by
+# only these individuals will no longer be present in the haplotype data, i.e.
+# the column sums for such SNVs will be zero.
+#which(Mutations$AC == 0)
 
-#from the following output rouhgly 93% of the SNVs are rare (AF <= 0.01) for chr22
-#Did we filter for rarity? Is it normal for so many SNVs to be rare?
-length(which(Mutations$afreq <= 0.01))/nrow(Mutations)
+library(Matrix)
+#which(colSums(Haplotypes) == 0)
 
-#Do we have data for every person, i.e. no missing values for any mutation
-unique(Mutations$NS)  #GOOD! no missing values
-unique(Mutations$AN)  #GOOD! no missing values
+# To save space, it makes sense to remove these SNVs from the data.
+# remove SNVs that are not carried by any member of the sample
+remove_cols <- which(colSums(Haplotypes) == 0)
+Haplotypes <- Haplotypes[, -remove_cols]
+Mutations <- Mutations[-remove_cols, ]
+Mutations$colID = seq(1:nrow(Mutations))
+#TODO: get rid of the variable colID. Really, what is the point of this
+#variable? SNVs should be in same order in both haplotypes and
+#mutations anyway....
 
-head(Mutations)
-Mutations <- Mutations[, c(1:5, 7:15, 17)]
+dimHaplotypes[[chrom_num]] = dim(Haplotypes)
+#Mutations <- Mutations[, c(1:5, 7:16)]
 
 #Assign object name based on chromosoem number
 object_name <- paste0("SNVdata_chrom", chrom_num, sep = "")
@@ -107,15 +111,20 @@ assign(object_name,
 save(SNVdata_chrom22,
      file = paste0("C:/Users/cnieuwoudt/Documents/GitHub/1000-Genomes-Exon-Data/Formatted-SNVdata/SNVdata_chrom", chrom_num, ".rda", sep = ""))
 
-# #store the formatted SNVdata object in the "Formatted SNVdata" folder
-# save(vcf_chrom21,
-#      file = paste0("C:/Users/cnieuwoudt/Documents/GitHub/1000-Genomes-Exon-Data/Formatted-SNVdata/vcf_chrom", chrom_num, ".rda", sep = ""))
+remove(SNVdata_chrom22)
+remove(genotypes)
+remove(Haplotypes)
+remove(Mutations)
+remove(muts)
+remove(vcf)
+remove(remove_cols)
 
-# remove(SNVdata_chrom9)
-#
-# #load the data from my computer, with a new name, to see if format was preserved
-# load(file = paste0("C:/Users/cnieuwoudt/Documents/GitHub/1000-Genomes-Exon-Data/Formatted SNVdata/SNVdata_chrom", chrom_num, ".rda", sep = ""))
+# After all chromomsomes processed
+# Quick quality checks
+all(sapply(SampID_order, identical, SampID_order[[1]]))
+all(sapply(dimHaplotypes, function(x){x[[1]]}) == 5052)
+all(sampLength == 2526)
 
-
-# #load SNVdata from github repository
-# load(url(paste0("https://github.com/cnieuwoudt/1000-Genomes-Exon-Data/raw/master/Formatted-SNVdata/SNVdata_chrom", chrom_num, ".rda", sep = "")))
+#number of retained SNVs in for each chromosome
+sapply(dimHaplotypes, function(x){x[[2]]})
+#NOTE: follow up with Wendy about chromosome 19
